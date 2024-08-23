ff242dbf8e5e08f3661db97718218c3e7e81b90a
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-08-23 13:10:11 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,111 +2,111 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="m-auto">
+      <nav className="h-20 w-10/12 m-auto flex justify-between">
+        <div id="menu-logo" className="my-2">
+          <img className="w-28" src="./logo-rama-global.png" alt="logo" />
         </div>
+       <div className="ml-20 flex">
+          <ul className="flex space-x-8 items-center font-corben text-[17px]">
+            <li><a href="">Início</a></li>
+            <li><a href="">Quem Somos</a></li>
+            <li><a href="">Clientes</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contato</a></li>
+          </ul>
+       </div>
+       <div className="flex w-1/3 items-center">
+        <button className="font-normal uppercase text-[14px] mr-0 bg-slate-950 ml-auto py-3 px-6 rounded-full text-white">
+          Solicitar um orçamento
+        </button>
+       </div>
+       <div className="flex space-x-4 text-trans items-center w-15">
+          <img className="w-5 cursor-pointer" src="./instagram.png" alt="instagram-logo" />
+          <img className="w-5 cursor-pointer" src="./facebook.png" alt="instagram-logo" />
+       </div>
+      </nav>
+      <div id="hero" className="m-auto mt-6 w-10/12">
+        <div>
+          <h1 className="text-6xl text-center m-auto mt-0 font-corben text-new-green w-2/3">
+            Conectando Sua Empresa ao Sucesso Sustentável
+          </h1>
+          <h2 className="text-xl text-center font-thin m-auto mt-4 font-corben text-new-green w-2/3">
+            Oferecemos consultoria especializada que entende a cultura única de sua empresa.
+          </h2>
+        </div>
+        <img src="hero-image.jpg" alt="arvores" className="w-1/2 rounded-3xl m-auto mt-10" />
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div id="services" className="bg-slate-50 mt-10 pb-4 w-full">
+        <h1 className="text-5xl text-center m-auto pt-5 font-corben text-new-green w-2/3">
+          Nossos Serviços
+        </h1>
+        <div className="flex flex-wrap m-auto justify-between w-10/12 mt-6">
+          <div className="bg-white border-[1px] border-lime-700 w-[540px] m-5 ml-10 rounded-2xl">
+            <h2 className="text-2xl text-center m-auto mt-6 font-corben text-gray-800">
+              Certificação FSC
+            </h2>
+            <img src="fsc-logo.png" alt="fsc-logo" className="w-[120px] m-auto mt-5" />
+            <p className="text-lg text-center m-auto mt-4 font-corben text-gray-600 px-8 pb-6">
+            A certificação FSC melhora a reputação de sua empresa, abre acesso a mercados conscientes, 
+            cumpre regulações ambientais e atrai consumidores que valorizam a sustentabilidade.
+            </p>
+            <div className="flex justify-center mb-5">
+              <button className="border-2 rounded-full border-orange-400 text-orange-400 px-4 py-2 mb-3 text-md hover:text-white hover:bg-orange-400">
+                Mais Informações
+              </button>
+            </div>
+          </div>
+          <div className="bg-white border-[1px] border-lime-700 w-[540px] m-5 mr-10 rounded-2xl">
+            <h2 className="text-2xl text-center m-auto mt-6 font-corben text-gray-800">
+              Certificação PEFC
+            </h2>
+            <img src="pefc-logo.png" alt="fsc-logo" className="w-[100px] m-auto mt-5" />
+            <p className="text-lg text-center m-auto mt-4 font-corben text-gray-600 px-8 pb-6">
+            A certificação PEFC demonstra manejo florestal sustentável, melhora a reputação de sua empresa, 
+            facilita acesso a mercados que exigem sustentabilidade e atrai consumidores preocupados 
+            com o meio ambiente.
+            </p>
+            <div className="flex justify-center mb-5">
+              <button className="border-2 rounded-full border-orange-400 text-orange-400 px-4 py-2 mb-3 text-md hover:text-white hover:bg-orange-400">
+                Mais Informações
+              </button>
+            </div>
+          </div>
+          <div className="bg-white border-[1px] border-lime-700 w-[540px] m-5 ml-10 rounded-2xl">
+            <h2 className="text-2xl text-center m-auto mt-6 font-corben text-gray-800">
+              Certificação ESG
+            </h2>
+            <img src="esg-logo.png" alt="fsc-logo" className="w-[120px] m-auto mt-5" />
+            <p className="text-lg text-center m-auto mt-4 font-corben text-gray-600 px-8 pb-6">
+            A certificação ESG avalia práticas empresariais em critérios ambientais, 
+            sociais e de governança, assegurando que a empresa opera de forma sustentável e ética, 
+            considerando seu impacto ambiental, responsabilidade social e práticas de gerenciamento 
+            transparentes e justas.
+            </p>
+            <div className="flex justify-center mb-5">
+              <button className="border-2 rounded-full border-orange-400 text-orange-400 px-4 py-2 mb-3 text-md hover:text-white hover:bg-orange-400">
+                Mais Informações
+              </button>
+            </div>
+          </div>
+          <div className="bg-white border-[1px] border-lime-700 w-[540px] m-5 mr-10 rounded-2xl">
+            <h2 className="text-2xl text-center m-auto mt-6 font-corben text-gray-800">
+            Certificação de Rótulo Ecológico
+            </h2>
+            <img src="rotulo-ecologico-logo.png" alt="fsc-logo" className="w-[140px] m-auto mt-5" />
+            <p className="text-lg text-center m-auto mt-4 font-corben text-gray-600 px-8 pb-6">
+            O certificado de Rótulo Ecológico diferencia produtos ambientalmente responsáveis, 
+            melhora a imagem da marca e abre mercados sustentáveis, atraindo consumidores que 
+            buscam opções ecológicas e ajudando-os a escolher produtos menos nocivos ao meio ambiente.
+            </p>
+            <div className="flex justify-center mb-5">
+              <button className="border-2 rounded-full border-orange-400 text-orange-400 px-4 py-2 mb-3 text-md hover:text-white hover:bg-orange-400">
+                Mais Informações
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
