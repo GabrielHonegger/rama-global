207510887c935afd204b799024f87877e09904e2
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-08-24 13:14:39 -0300

    fix crossOrigin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
       <link rel="icon" href="/icon.ico"/>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link href="https://fonts.googleapis.com/css2?family=Corben:wght@400;700&display=swap" rel="stylesheet"/>
       </head>
       <body className={inter.className}>{children}</body>
