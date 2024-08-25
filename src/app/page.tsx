339a355cca2573362689988835a2bbffe2ba99a6
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-08-25 13:44:29 -0300

    add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -229,8 +229,8 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div id="whatsapp" className="fixed right-10 bottom-10 bg-green-whatapp p-4 rounded-full">
-        <a aria-label="Chat on WhatsApp" className="cursor-pointer" href="https://wa.me/554198730077?text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20a%20consultoria" target="_blank">
+      <div id="whatsapp">
+        <a aria-label="Chat on WhatsApp" className="fixed right-7 bottom-7 bg-green-whatapp p-4 rounded-full cursor-pointer" href="https://wa.me/554198730077?text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20a%20consultoria" target="_blank">
         <img alt="Chat on WhatsApp" src="whatsapp.png" className="w-8" />
       </a>
       </div>
