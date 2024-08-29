e7462deffc25be9602c3f756dacea7bb3350f434
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-08-29 11:31:52 -0300

    add post page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Blog = () => {
   return (
     <main className="m-auto">
-      <div className="m-auto mt-3 lg:mt-6 w-full lg:w-10/12 p-6">
+      <div className="m-auto mt-3 lg:mt-6 w-full lg:w-10/12 px-6 lg:py-4">
             <h1 className='lg:text-4xl md:text-3xl  text-center mb-10 text-2xl'>
               Blog
             </h1>
@@ -15,6 +15,24 @@ const Blog = () => {
                   title='Como Surgiu o FSC?'
                   preview='O FSC foi criado como resposta a uma preocupação internacional com o destino das florestas mundiais no início da década de'
                   img='blog-post1' />
+              </Link>
+              <Link href="/">
+                <ThreeDCard
+                  title='O Que Significa FSC?'
+                  preview='O FSC segue princípios e critérios que, aplicados ao manejo florestal, garantem que os produtos originários de uma determinada'
+                  img='blog-post2' />
+              </Link>
+              <Link href="/">
+                <ThreeDCard
+                  title='5 Passos para a Obtenção do Selo FSC'
+                  preview='Para que uma empresa possa receber a certificação FSC®, vários requisitos precisam ser observados. Elaboramos aqui uma'
+                  img='blog-post3' />
+              </Link>
+              <Link href="/">
+                <ThreeDCard
+                  title='Quem Pode Utilizar o Selo FSC?'
+                  preview='Esta é uma das perguntas mais freqüentes que as pessoas nos fazem sobre o sistema de certificação do FSC. A dúvida surge principalmente'
+                  img='blog-post4' />
               </Link>
             </div>
         </div>
