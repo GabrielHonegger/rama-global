bee791c55564c0f427db5ee367552368db49df63
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-12-04 09:58:44 -0300

    add analytics and tag manager CSP

--- next.config.mjs
+++ next.config.mjs
@@ -11,12 +11,12 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com https://www.google.com https://www.gstatic.com https://www.googletagmanager.com https://www.google-analytics.com https://vercel.live;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' data: https://www.googletagmanager.com;
-              connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com https://www.google.com https://www.gstatic.com https://www.googletagmanager.com https://www.google-analytics.com https://vercel.live https://www.googleadservices.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.googletagmanager.com;
+              img-src 'self' data: https://www.googletagmanager.com https://fonts.gstatic.com;
+              connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://www.google.com.br;
               font-src 'self' https://fonts.gstatic.com;
-              frame-src 'self' https://www.google.com https://www.googletagmanager.com;
+              frame-src 'self' https://www.google.com https://www.googletagmanager.com https://td.doubleclick.net;
               frame-ancestors 'none';
               object-src 'none';`
               .replace(/\s{2,}/g, " ")
